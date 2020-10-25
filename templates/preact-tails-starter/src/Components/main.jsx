48acc311e202652aa656bd479b29f7f47faacade
anup-a <aglawe.anup22@gmail.com>
2020-10-25 22:44:10 +0530

    fix styles

--- templates/preact-tails-starter/src/Components/main.jsx
+++ templates/preact-tails-starter/src/Components/main.jsx
@@ -6,7 +6,7 @@ import TailwindSVG from './../assets/tailwindcss.svg'
 function Main() {
   return (
     <div className="flex flex-col items-center p-6 rounded-lg shadow-lg justify-evenly w-7/10 h-7/10 dark:bg-darkish-black">
-      <div className="flex items-center w-full bg-green-300 justify-evenly">
+      <div className="flex items-center w-full justify-evenly">
         <div className="preact-svg">
           <img src={PreactSVG} alt="" width="150" />
         </div>
